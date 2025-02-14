ca8bd91dfc253d2e91121376916544c2041cda39
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-02-14 13:57:49 -0600

    big refactor

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
-import { WinsAnalysis } from "./analyzers/WinsAnalysis";
-import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
-import { ConsoleReport } from "./reportTargets/ConsoleReport";
 import { Summary } from "./Summary";
 
-const csvFileReader = new CsvFileReader("football.csv");
+const summary = Summary.winsAnalysisWithHtmlReport("Man United");
+const matchReader = MatchReader.fromCsv("football.csv");
 
-const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-
-const summary = new Summary(
-  new WinsAnalysis("Man United"),
-  new ConsoleReport()
-);
-
 summary.buildAndPrintReport(matchReader.matches);
